90fa07e9cd99137696108f9b8f4d210e6a6f9eed
Gonçalo Lopes <goncalo.capela.lopes@gmail.com>
2026-01-12 21:32:23 +0000

    Update gitignore and comment not yet developed sagemaker stack

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from aws_cdk import App, Environment
 
-from src.infra.sagemaker_stack import SageMakerStack
+#from src.infra.sagemaker_stack import SageMakerStack
 from src.infra.storage_stack import StorageStack
 
 app = App()
@@ -11,7 +11,7 @@ env = Environment(
     region=os.environ.get("CDK_DEFAULT_REGION", "eu-west-1")
 )
 
-sagemaker_stack = SageMakerStack(app, "SageMakerStack", env=env)
+#sagemaker_stack = SageMakerStack(app, "SageMakerStack", env=env)
 storage_stack = StorageStack(app, "StorageStack", env=env)
 
 app.synth()
