bf9326542f2a8e7cf5061c53b6f4db0ed82ed342
Gonçalo Lopes <goncalo.capela.lopes@gmail.com>
2026-01-12 22:43:39 +0000

    Add default vpc fetch for sagemaker domain

--- src/infra/sagemaker_stack.py
+++ src/infra/sagemaker_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import Stack
 from aws_cdk import aws_sagemaker as sagemaker
 from aws_cdk import aws_iam as iam
 from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_ec2 as ec2
 from constructs import Construct
 
 class SageMakerStack(Stack):   
@@ -20,6 +21,19 @@ class SageMakerStack(Stack):
         
         # Grant read/write permissions to the data bucket
         data_bucket.grant_read_write(self.sagemaker_role)
+        
+        # Look up the default VPC
+        vpc = ec2.Vpc.from_lookup(
+            self,
+            "DefaultVPC",
+            is_default=True
+        )
+
+        # Get subnet IDs from the VPC
+        subnet_ids = [subnet.subnet_id for subnet in vpc.private_subnets]
+        # If no private subnets, use public subnets
+        if not subnet_ids:
+            subnet_ids = [subnet.subnet_id for subnet in vpc.public_subnets]
 
         # SageMaker Domain
         self.domain = sagemaker.CfnDomain(
@@ -27,6 +41,8 @@ class SageMakerStack(Stack):
             "SageMakerDomain",
             domain_name="Labyrinth",
             auth_mode="IAM",
+            vpc_id=vpc.vpc_id,
+            subnet_ids=subnet_ids,
             app_network_access_type="PublicInternetOnly",
             default_user_settings=sagemaker.CfnDomain.UserSettingsProperty(
                 execution_role=self.sagemaker_role.role_arn
