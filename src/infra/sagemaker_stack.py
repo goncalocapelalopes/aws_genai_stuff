405aaf9cf5166b480139081cc5f126c7cf2b2ec2
Gonçalo Lopes <goncalo.capela.lopes@gmail.com>
2026-01-12 22:34:07 +0000

    Add sagemaker stack

---
+++ src/infra/sagemaker_stack.py
@@ -0,0 +1,44 @@
+from aws_cdk import Stack
+from aws_cdk import aws_sagemaker as sagemaker
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_s3 as s3
+from constructs import Construct
+
+class SageMakerStack(Stack):   
+    def __init__(self, scope: Construct, construct_id: str, data_bucket: s3.IBucket, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # IAM Role for SageMaker
+        self.sagemaker_role = iam.Role(
+            self,
+            "SageMakerExecutionRole",
+            assumed_by=iam.ServicePrincipal("sagemaker.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSageMakerFullAccess")
+            ]
+        )
+        
+        data_bucket.grant_read_write(self.sagemaker_role)
+
+        # SageMaker Domain
+        self.domain = sagemaker.CfnDomain(
+            self,
+            "SageMakerDomain",
+            domain_name="Labyrinth",
+            auth_mode="IAM",
+            app_network_access_type="PublicInternetOnly",
+            default_user_settings=sagemaker.CfnDomain.UserSettingsProperty(
+                execution_role=self.sagemaker_role.role_arn
+            )
+        )
+        
+        #User Profile
+        self.user_profile = sagemaker.CfnUserProfile(
+            self,
+            "SageMakerUserProfile",
+            domain_id=self.domain.attr_domain_id,
+            user_profile_name="gon",
+            user_settings=sagemaker.CfnUserProfile.UserSettingsProperty(
+                execution_role=self.sagemaker_role.role_arn
+            )
+        )
